ffb466f8120a2e7500a4fb0b3b0831c9132a4051
maykongpedro <maykonglaffite@gmail.com>
2021-09-04 22:05:34 -0300

    add rascunho primeiro gráfico

--- data-raw/20210903-bird-baths-week-36-2021.R
+++ data-raw/20210903-bird-baths-week-36-2021.R
@@ -10,13 +10,13 @@
 # article Avian Assemblages at Bird Baths: A Comparison of Urban and 
 # Rural Bird Baths in Australia.
 
-tuesdata <- tidytuesdayR::tt_load(2021, week = 36)
-bird_baths <- tuesdata$bird_baths
-bird_baths
+# tuesdata <- tidytuesdayR::tt_load(2021, week = 36)
+# bird_baths <- tuesdata$bird_baths
+# bird_baths
 
 
 # Salvar dados ------------------------------------------------------------
-bird_baths %>% saveRDS("./data/2021-08-31-bird-baths.RDS")
+# bird_baths %>% saveRDS("./data/2021-08-31-bird-baths.RDS")
 
 
 # Abrir dados salvos ------------------------------------------------------
@@ -40,29 +40,30 @@ db_bird_baths %>%
     dplyr::summarise(n = dplyr::n())
 
 
-# visualizar no excel para ter certeza se fiz corretamente
-db_bird_baths %>%
-    dplyr::group_by(urban_rural) %>%
-    viewxl::view_in_xl()
+# visualizar no excel
+# db_bird_baths %>%
+#     dplyr::group_by(urban_rural) %>%
+#     viewxl::view_in_xl()
         
 
 # pensar em como juntar esses três itens em apenas uma base ou
 # montar uma função
 
-# Espécies mais comuns no meio urbano
-db_bird_baths %>% 
-    dplyr::filter(urban_rural == "Rural") %>% 
-    dplyr::group_by(bird_type) %>% 
+# Espécies mais comuns no meio urbano e rural
+especies_por_local <- db_bird_baths %>% 
+    #dplyr::filter(urban_rural == "Rural") %>% 
+    dplyr::filter(!is.na(urban_rural)) %>% 
+    dplyr::group_by(urban_rural, bird_type) %>% 
     dplyr::summarise(qtd_birds = sum(bird_count)) %>% 
     dplyr::arrange(dplyr::desc(qtd_birds))
 
 
 # Espécies mais comuns no meio rural
-db_bird_baths %>% 
-    dplyr::filter(urban_rural == "Urban") %>% 
-    dplyr::group_by(bird_type) %>% 
-    dplyr::summarise(qtd_birds = sum(bird_count)) %>% 
-    dplyr::arrange(dplyr::desc(qtd_birds))
+# db_bird_baths %>% 
+#     dplyr::filter(urban_rural == "Urban") %>% 
+#     dplyr::group_by(bird_type) %>% 
+#     dplyr::summarise(qtd_birds = sum(bird_count)) %>% 
+#     dplyr::arrange(dplyr::desc(qtd_birds))
 
 
 # Espécies mais comum na base
@@ -78,4 +79,18 @@ db_bird_baths %>%
 # pensei em algo como o gráfico dos pinguins, com imagens dos passáros
 # coladas no gráfico. Acho que gráficos simples de barras já servem
 
-
+especies_por_local %>% 
+    dplyr::filter(urban_rural == "Urban") %>% 
+    dplyr::slice(1:15) %>% 
+    dplyr::mutate(
+        bird_type = forcats::fct_reorder(bird_type, qtd_birds)
+    ) %>% 
+    ggplot2::ggplot()+
+    ggplot2::geom_col(
+        ggplot2::aes(
+            x = qtd_birds,
+            y = bird_type
+        )
+    )+
+    ggplot2::theme_minimal()
+                      
